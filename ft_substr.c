aaa21a711a792e58cb070476033823e6f948e3f5
Akihiro Tajima <atajima@c5r2s4.42tokyo.jp>
2026-05-04 19:49:46 +0900

    libft

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,13 @@
 /*   By: atajima <atajima@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 14:17:58 by atajima           #+#    #+#             */
-/*   Updated: 2026/05/04 18:41:39 by atajima          ###   ########.fr       */
+/*   Updated: 2026/05/04 19:49:18 by atajima          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*copy(unsigned int start, size_t len, size_t s_len);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -24,58 +26,61 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	while (s[s_len])
 		s_len++;
-	res = copy(s, start, len, s_len);
-	if (*res == '\0')
+		printf ("%zu\n", s_len);
+	res = copy(start, len, s_len);
+	if (!res)
+		return (NULL);
+	if (start >= s_len)
 		return (res);
-	while (s[s_len + i] && i < len)
+	while (s[start + i] && i < len)
 	{
-		
+		res[i] = s[start + i];
+		i++;
 	}
+	res[i] = '\0';
+	return (res);
 }
 
-char	*copy(char const *s, unsigned int start, size_t len, size_t s_len)
+char	*copy(unsigned int start, size_t len, size_t s_len)
 {
 	char	*res;
 
-	if (s_len - 1 <= start)
+	if (s_len <= start)
 	{
 		res = malloc(1);
 		if (!res)
 			return (NULL);
 		*res = '\0';
 	}
-	else if (s_len - 1 < start + len)
+	else if (s_len - start < len)
 	{
-		res = malloc(sizeof(char) * (start + len - s_len + 2));
+		res = malloc(sizeof(char) * (s_len - start + 1));
 		if (!res)
 			return (NULL);
 	}
 	else
 	{
-	res = malloc(sizeof(char) * (start + len + 1));
-	if (!res)
-		return (NULL);
+		res = malloc(sizeof(char) * (len + 1));
+		if (!res)
+			return (NULL);
 	}
 	return (res);
 }
 
+int	main(int argc, char **argv)
+{
+	size_t		start;
+	size_t		len;
+	char	*sub;
 
-// #include <stdio.h>
-
-// int	main(int argc, char **argv)
-// {
-// 	int		start;
-// 	int		len;
-// 	char	*sub;
-
-// 	if (argc != 4)
-// 		return (0);
-// 	start = atoi(argv[2]);
-// 	len = atoi(argv[3]);
-// 	sub = ft_substr(argv[1], start, len);
-// 	if (!sub)
-// 		return (1);
-// 	printf ("%s\n", sub);
-// 	free(sub);
-// 	return (0);
-// }
+	if (argc != 4)
+		return (0);
+	start = atoi(argv[2]);
+	len = atoi(argv[3]);
+	sub = ft_substr(argv[1], start, len);
+	if (!sub)
+		return (1);
+	printf ("%s\n", sub);
+	free(sub);
+	return (0);
+}
